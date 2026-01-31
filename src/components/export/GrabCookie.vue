72f8272490f10c60f6844d08dfca2b8495a3534f
Trương Khánh <truongnguyenkhanh230800@gmail.com>
2026-01-31 10:11:15 +0700

    [wwa-002] on update cookie handle something

--- src/components/export/GrabCookie.vue
+++ src/components/export/GrabCookie.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <TextArea label="Cookie" placeholder="Enter cookie value..." />
+    <TextArea
+      v-model="cookie"
+      label="Cookie"
+      placeholder="Enter cookie value..."
+    />
   </div>
 </template>
 
 <script setup>
-import TextArea from '../forms/FormElements/TextArea.vue';
+import { ref, watch } from 'vue'
+import TextArea from '../forms/FormElements/TextArea.vue'
+
+const cookie = ref('')
+
+watch(cookie, (val) => {
+  if (val && val.trim() !== '') {
+    console.log('Cookie value:', val)
+  }
+})
 </script>
-
-<style>
-
-</style>
