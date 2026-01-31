cb86af76642cf6ceee3f8000d02cd3008949e291
Trương Khánh <truongnguyenkhanh230800@gmail.com>
2026-01-31 14:15:40 +0700

    [wwa-002] GrabSelectDate

---
+++ src/components/export/GrabSelectDate.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="w-full">
+    <!-- container -->
+    <div class="flex flex-col gap-4 w-full">
+      <!-- label + input -->
+      <div class="flex flex-col gap-2 w-full">
+        <label
+          class="block text-sm font-medium text-gray-700 dark:text-gray-400"
+        >
+          Select Date(s)
+        </label>
+
+        <flat-pickr
+          v-model="rangeDate"
+          :config="multiDateConfig"
+          class="h-10 w-full border border-gray-300 rounded px-2 py-1 text-sm dark:bg-gray-900 dark:border-gray-700 dark:text-white/90"
+          placeholder="Select multiple dates"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import flatPickr from 'vue-flatpickr-component'
+import 'flatpickr/dist/flatpickr.css'
+
+const multiDateConfig = {
+  mode: 'multiple',
+  dateFormat: 'd/m/Y',
+  altInput: true,
+  altFormat: 'd/m/Y',
+  allowInput: true,
+  conjunction: ', ',
+}
+
+const rangeDate = ref([])
+</script>
--- src/views/Export/GrabExport.vue
+++ src/views/Export/GrabExport.vue
@@ -1,16 +1,17 @@
 <template>
   <AdminLayout>
     <PageBreadcrumb :pageTitle="currentPageTitle" />
-    <div class="grid grid-cols-12 gap-6">
+    <div class="grid grid-cols-12 gap-4">
       <div class="col-span-12">
         <div
           class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]"
         >
           <div class="px-4 py-4 sm:pl-6 sm:pr-4 flex flex-col gap-4 min-h-screen">
             <GrabCookie />
-            <div class="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-              <div class="w-full">
+            <div class="w-full">
+              <div class="flex flex-col gap-4">
                 <GrabSelectBranch />
+                <GrabSelectDate />
               </div>
             </div>
           </div>
@@ -26,7 +27,7 @@ import AdminLayout from '@/components/layout/AdminLayout.vue'
 import PageBreadcrumb from '@/components/common/PageBreadcrumb.vue'
 import GrabSelectBranch from '@/components/export/GrabSelectBranch.vue'
 import GrabCookie from '@/components/export/GrabCookie.vue'
-import VuexSample from '@/components/VuexSample.vue'
+import GrabSelectDate from '@/components/export/GrabSelectDate.vue'
 
 const currentPageTitle = ref('Grab Export')
 </script>
