99afb99832949dfb634d0fda32125d8c4abcdbdb
Trương Khánh <truongnguyenkhanh230800@gmail.com>
2026-01-31 10:02:09 +0700

    [wwa-002] use ChevronDownIcon in GrabSelectBranch

--- src/components/export/GrabSelectBranch.vue
+++ src/components/export/GrabSelectBranch.vue
@@ -16,29 +16,14 @@
       <span
         class="absolute z-30 text-gray-500 -translate-y-1/2 pointer-events-none right-4 top-1/2 dark:text-gray-400"
       >
-        <svg
-          class="stroke-current"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M4.79175 7.396L10.0001 12.6043L15.2084 7.396"
-            stroke=""
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
+        <ChevronDownIcon />
       </span>
     </div>
   </div>
 </template>
 <script setup>
 import { ref } from 'vue'
-
+import { ChevronDownIcon } from '@/icons';
 const selectedBranch = ref('all')
 const accounts = ref([])
 </script>
